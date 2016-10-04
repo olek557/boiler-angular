abb182c05608c588b734c5a926699fe2d78327cd
Oleksiy Derkach <oleksiy.derkach@perfectial.com>
2016-10-04 19:54:17 +0300

    working with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,20 @@
 var gulp = require('gulp');
-var concatCss = require('gulp-concat-css');
-var minifyCss = require('gulp-minify-css');
+var concatCSS = require('gulp-concat-css');
+var minifyCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
+var notify = require('gulp-notify');
 var autoprefixer = require('gulp-autoprefixer');
-var livereload = require('gulp-livereload');
-var connect = require('gulp-connect');
-var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
-var imagemin = require('gulp-imagemin');
-var pngquant = require('imagemin-pngquant');
 
-gulp.task('sass', function() {
- return gulp.src('./sass/**/*.scss')
-  .pipe(sourcemaps.init())
-  .pipe(sass().on('error', sass.logError))
-  .pipe(sourcemaps.write('./maps'))
-  .pipe(gulp.dest('./css'));
+gulp.task('default', function () {
+  return gulp.src('css/**/*.css')
+    .pipe(concatCSS('main.css'))
+    .pipe(autoprefixer({browsers: ['last 4 versions', '> 1%', 'ie 9']}))
+    .pipe(minifyCSS())
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest('css/'))
+    .pipe(notify('Done'));
 });
 
-gulp.task('watch', function() {
-    gulp.watch("./scss/**/*.scss", ['sass']);
-    gulp.watch("./css/**/*.css", ['css']);
-    gulp.watch("./*.html", ['html']);
-    gulp.watch("./js/*.js", ['js']);
-    gulp.watch("./js/libs/*.js", ['jslibs']);
-    gulp.watch("./js/modules/**/*.js", ['jsmods']);
-});
-
-gulp.task('default', ['sass', 'watch']);
+gulp.task('watch', function(){
+  gulp.watch('css/*.css', ['default']);
+})
