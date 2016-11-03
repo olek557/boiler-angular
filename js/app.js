58982995259d1e30198db9802050eb5f317ba165
Oleksiy Derkach <oleksiy.derkach@perfectial.com>
2016-11-03 09:49:52 +0200

    fix get request

--- js/app.js
+++ js/app.js
@@ -1,26 +1,22 @@
-'use strict';
-
-var app = angular.module('myApp', []);
-var some = [];
-
-app.controller('AdviserController', function($scope, $http) {
-    $scope.items = {};
-    this.advise = $scope.items;
-    var advise = this.advise;
-    $scope.getItems = function() {
-     $http.get( "http://134.249.116.199:3000/api/a96ed253-fb2c-4ff6-9e37-ff1c5935b7ca/movies/4")
-        .success(function(data){
-            $scope.items.item = data;
-            $scope.items.item.poster_url = "images/poster_database/" + $scope.items.item.id + ".jpg";
-        });
-    }
-
-    $scope.getItems();
-    console.log(advise); 
+// 'use strict';
 
 
-});
+    var app = angular.module('myApp', []);
+    var some = [];
 
-app.controller('MainCntl', function($scope, $http) {
+    app.controller('AdviserController', function($scope, $http) {
+        var film = this;
+        film.items = [];
+        film.items[0] = "Test";
+        $scope.filmScope = [];
+        // that.item;
+         $http.get( "http://134.249.116.199:3000/api/a96ed253-fb2c-4ff6-9e37-ff1c5935b7ca/movies/4")
+            .then(function(responce){
+                $scope.film = responce.data;
+                console.log(responce.data);
+            });
+    });
+    // app.controller('MainCntl', function($scope, $http) {
 
-});
+    // });
+// });
