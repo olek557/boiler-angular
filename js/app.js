550f7dcac96f11d8b94a4dcfa6a9764c371c2483
Oleksiy Derkach <oleksiy.derkach@perfectial.com>
2016-11-13 23:22:41 +0200

    started work with fiml-board

--- js/app.js
+++ js/app.js
@@ -8,9 +8,11 @@
         var film = this;
         film.items = [];
         film.items[0] = "Test";
+        var rand = 1 + Math.floor(Math.random() * (100 + 1 - 1));
+        var film_link = "https://134.249.116.199:3000/api/a96ed253-fb2c-4ff6-9e37-ff1c5935b7ca/movies/" + rand;
         $scope.filmScope = [];
         // that.item;
-         $http.get( "http://134.249.116.199:3000/api/a96ed253-fb2c-4ff6-9e37-ff1c5935b7ca/movies/4")
+         $http.get( film_link )
             .then(function(responce){
                 $scope.film = responce.data;
                 console.log(responce.data);
